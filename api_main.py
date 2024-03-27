78c68367407a93e59b97f1d42596ccce3f7f3f10
Pablo Rosales <parosales@gmail.com>
2024-03-27 16:38:51 -0600

    moving code base from pythonProject dir into base dir of the repo

---
+++ api_main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+import model.model
+from joblib import load
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import json
+import model.data
+import pickle
+
+
+model_path = './model/'
+model_file = 'model.joblib'
+one_hot_encoder_file = 'one_hot_encoder_file.pickle' # one-hot encoder for trained data
+
+
+class SubjectAttributes(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: float
+    capital_loss: float
+    hours_per_week: int
+    native_country: str
+
+# model and encoder saved in the training phase
+model_obj = load(model_path + model_file)
+with open(model_path + one_hot_encoder_file, 'rb') as f:
+    oh_encoder = pickle.load(f)
+
+# Instantiate the app.
+app = FastAPI()
+
+@app.get("/")
+async def welcome_to_api():
+    return {"This is the API to the Prediction Model, welcome!"}
+
+#@app.post("/predictions")
+#async def exercise_function(the_body: SubjectAttributes):
+#    return {"body": the_body}
+
+@app.post("/predictions")
+async def predict_salary_over50(test_subject: SubjectAttributes):
+    cat_features = ['workclass', 'education', 'marital_status', 'occupation', 'relationship', 'race',
+                     'native_country', 'sex']
+
+    test_subject_as_json = json.dumps(test_subject.__dict__)
+    data_dict = json.loads(test_subject_as_json)
+    subject_df = pd.DataFrame.from_records([data_dict])
+    #print (subject_df)
+
+    test_subject, y, encoder, lb = model.data.process_data (
+        X = subject_df,
+        categorical_features = cat_features,
+        label = None,
+        training = False,
+        encoder = oh_encoder,
+        lb = None
+    )
+
+    salary_over50 = model.model.inference(model_obj, test_subject ) [0]
+    # print( salary_over50 )
+    return int(salary_over50)
+
---
+++ model/training_pipeline.py
@@ -0,0 +1,83 @@
+# ========================================= Setup ========================================= #
+
+# ----------------------------------- Import Libraries ----------------------------------- #
+import data, model
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, label_binarize, LabelBinarizer
+from sklearn import svm
+from joblib import dump
+import pickle
+
+
+# ------------------------------- Define vars and constants ------------------------------- #
+categorical_features = [ 'workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'native-country', 'sex' ]
+label = 'salary'
+lb = LabelBinarizer()
+
+# paths and files
+model_path = './model/'
+model_file = 'model.joblib'
+test_set_path = './test/'
+test_file = 'tests.py'
+one_hot_encoder_file = 'one_hot_encoder_file.pickle' # one-hot encoder for trained data
+
+# -------------------------------Functions ------------------------------- #
+def evaluate_on_categorical_slices(clean_data_df, model_obj, encoder, lb):
+#def evaluate_on_categorical_slices(clean_data_df, model_obj, lb):
+    for col in categorical_features:
+        for tested_value in clean_data_df[col].unique():
+            print (col, tested_value)
+
+            X, y, encoder, lb = data.process_data(
+                clean_data_df [ clean_data_df[col] == tested_value ],
+                categorical_features,
+                label,
+                False,
+                encoder,
+                lb
+            )
+
+            preds = model.inference (model_obj, X)
+            precision, recall, fbeta = model.compute_model_metrics(y, preds)
+            print ( f"precision, recall, fbeta: {precision}, {recall}, {fbeta}" )
+
+
+# ============================== Ingestion and pre-processing ============================== #
+clean_data_df = pd.read_csv( './data/clean_census.csv' )
+
+# -------------------------------------- Pre-processing ------------------------------------- #
+X, y, encoder_for_trained_data, lb = data.process_data(
+    clean_data_df ,
+    categorical_features,
+    label,
+    True
+)
+
+# save the OHE for other uses: e.g. prediction/inference
+with open(model_path + one_hot_encoder_file, 'wb') as f:
+    pickle.dump(encoder_for_trained_data, f)
+
+# ====================================== Model Training ====================================== #
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=23)
+model_obj = model.train_model( X_train, y_train )
+print ( type(model_obj) )
+dump(model_obj, model_path + model_file)
+
+#save for further tests
+#np.save(test_set_path + test_file, X_test)
+
+# ====================================== Evaluate Overall performance ====================================== #
+preds = model.inference(model_obj, X_test)
+precision, recall, fbeta = model.compute_model_metrics(y_test, preds)
+print ( f"precision, recall, fbeta: {precision}, {recall}, {fbeta}" )
+
+
+# ====================================== Evaluate on Slices of data ====================================== #
+#evaluate_on_categorical_slices(clean_data_df, model_obj, encoder_for_trained_data, lb)
+#evaluate_on_categorical_slices(clean_data_df, model_obj, lb)
+
+# ====================================== Predict ====================================== #
+preds = model.inference(model_obj, X_test)
+print(preds)
