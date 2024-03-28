9985b538314faa789610c86467b35ac555684b1f
Pablo Rosales <parosales@gmail.com>
2024-03-27 19:01:19 -0600

    correcting the code in test_public_api.py to handle posts to public api

--- test_public_api.py
+++ test_public_api.py
@@ -1,8 +1,12 @@
 import requests
+import json
 
-live_api_url = 'https://mlops-nanodegree-course-4-project.onrender.com/predictions'
+live_post_api_url = 'https://mlops-nanodegree-course-4-project.onrender.com/predictions'
+live_get_api_url = 'https://mlops-nanodegree-course-4-project.onrender.com/'
 
-test_data = {
+
+
+test_data = json.dumps({
     "age": 43,
     "workclass": "Self-emp-not-inc",
     "fnlgt": 292175,
@@ -17,11 +21,16 @@ test_data = {
     "capital_loss": 0,
     "hours_per_week": 45,
     "native_country": "United-States"
-}
+})
 
-response = requests.post(live_api_url, data=test_data)
+#response =requests.get(live_get_api_url).content
+#print (response)
+
+response = requests.post(live_post_api_url, data=test_data)
+print (response)
 
 if response.status_code == 200:
     print("POST request successful")
+    print (response.text)
 else:
     print("POST request failed")
